594f63cc7f1eeff20295edc406912bb8ff9f2c3a
VIRGZ77 <55164221+VIRGZ77@users.noreply.github.com>
2022-06-08 13:20:18 +0100

    Update ClassMarks.py

--- ClassMarks.py
+++ ClassMarks.py
@@ -7,12 +7,74 @@ subject = 2
 mark = 3
 grade = 4
 
+def format_record_write(record):
+    record = record.replace(",","") 
+    record = record.split()
+    return record[forename] + ', ' + record[surname] + ', ' + record[subject] + ', ' + record[mark] + '\n'
+
+def format_record_read(record):
+    record = record.replace("\n","")   
+    record = record.split(', ')
+    record.append(get_grade(record[mark]))   
+    return record
+
+def retrieve_records(filename):
+    with open(filename) as f:
+        all_records = f.readlines()
+        records = []
+        record = 0
+        for record in all_records:
+            records.append(format_record_read(record))
+    return records
+
+def get_grade(mark):
+    if (int(mark) >= 70): return 'A'
+    elif (int(mark) >= 60) and (int(mark) < 70): return 'B'
+    elif (int(mark) >= 45) and (int(mark) < 60): return 'C'    
+    elif (int(mark) < 45): return 'F'
+
+def student_report(student_name):
+    student_name = student_name.split()        
+    print("Results for:")
+    print("\t", student_name[forename], student_name[surname], "\t", end="")
+    r = 0
+    while r < len(records):
+        if (records[r][forename] == student_name[forename]) and (records[r][surname]) == student_name[surname]:
+            print('[', records[r][subject],':',records[r][mark], records[r][grade], '] ', end="")
+        r+=1
+
+def subject_report(subject_name):
+    subject_name = subject_name.upper()
+    print("\n") 
+    marks_list = []
+    pass_count = 0
+    i = 0             
+    if (subject_name == 'MATHS') or (subject_name == 'ENGLISH') or (subject_name == 'SCIENCE') or (subject_name == 'ART'):
+        print(subject_name, "Exam Results Statistics:")
+        while i < len(records):
+            if (records[i][subject].upper() == subject_name):
+                marks_list.append(int(records[i][mark]))
+                if not(records[i][grade] == '(F)'):
+                    pass_count+=1
+            i+=1     
+    else: 
+        print("ALL Exam Results Statistics:")
+        while i < len(records):
+            marks_list.append(int(records[i][mark]))
+            if not(records[i][grade] == '(F)'):
+                pass_count+=1
+            i+=1                       
+    print("min:", min(marks_list))
+    print("max:", max(marks_list))
+    print("mean:", statistics.mean(marks_list))
+    print("median:", statistics.median(marks_list)) 
+    print("Pass rate:", pass_count, '/', len(records))
+
 # Promt User to enter a class name.
 classname = input("Please enter the Class name: ")
 filename = classname + '.txt'
 if results_exist(filename):
     # If it exists, read all entered records into program
-    print("Retrieving existing file data")
     with open(filename) as f:
         all_records = f.readlines()
 f = open(filename, 'a+') 
@@ -22,71 +84,31 @@ user_mode = input("Please select mode. ENTRY or REPORT? ")
 if user_mode.upper() == 'ENTRY':   
     entry_mode = True
     report_mode = False 
-    print("\nYou have entered ENTRY mode")
-    print("Enter students <FORENAME> <SURNAME> <SUBJECT> <MARK> - seperated by <SPACES>")
 if user_mode.upper() == 'REPORT':
     report_mode = True
     entry_mode = False
 
 while entry_mode:
+    print("Enter students <FORENAME> <SURNAME> <SUBJECT> <MARK> - seperated by <SPACES>")
     user_input = input("Enter Result: ")
+    
     if user_input[0:4].upper() == 'EXIT':
         f.close()
         entry_mode = False
     else:
-        new_record = user_input.replace(",","")
-        new_record = new_record.split()
-        write_record = new_record[forename] + ', ' + new_record[surname] + ', ' + new_record[subject] + ', ' + new_record[mark]
-        f.write(write_record + '\n')        
+        f.write(format_record_write(user_input))
 
 while report_mode: 
-    with open(classname + '.txt') as f:
-        all_records = f.readlines()
-        records = []
-        for record in all_records:
-            record = record.replace("\n","")   
-            record = record.split(', ')
-            if (int(record[mark]) >= 70): record.append('(A)')
-            elif (int(record[mark]) >= 60) and (int(record[mark]) < 70): record.append('(B)')
-            elif (int(record[mark]) >= 45) and (int(record[mark]) < 60): record.append('(C)')    
-            elif (int(record[mark]) < 45): record.append('(F)') 
-            records.append(record)
-            
+    records = retrieve_records(filename)
+    
     report_type = input("Please select report. STUDENT or SUBJECT? ")
+    
     if report_type.upper() == 'STUDENT':
         user_input = input("Enter Students name: ")
-        student_name = user_input.split()        
-        print("Results for:")
-        print("\t", student_name[forename], student_name[surname], "\t", end="")
-        r = 0
-        while r < len(records):
-            if (records[r][forename] == student_name[forename]) and (records[r][surname]) == student_name[surname]:
-                print('[', records[r][subject],':',records[r][mark], records[r][grade], '] ', end="")
-            r+=1
+        student_report(user_input)
+        
     if report_type.upper() == 'SUBJECT':
         user_input = input("Please select a subject. ALL MATHS ENGLISH SCIENCE ART? ")
-        subject_report = user_input.upper()
-        print("\n") 
-        print(subject_report, "Exam Results Statistics:")
-        marks_list = []
-        pass_count = 0
-        i = 0             
-        if subject_report == 'ALL':
-            while i < len(records):
-                marks_list.append(int(records[i][mark]))
-                if not(records[i][grade] == '(F)'):
-                    pass_count+=1
-                i+=1                   
-        else:
-            while i < len(records):
-                if (records[i][subject].upper() == subject_report):
-                    marks_list.append(int(records[i][mark]))
-                    if not(records[i][grade] == '(F)'):
-                        pass_count+=1
-                i+=1                        
-        print("min:", min(marks_list))
-        print("max:", max(marks_list))
-        print("mean:", statistics.mean(marks_list))
-        print("median:", statistics.median(marks_list)) 
-        print("Pass rate:", pass_count, '/', len(records))
+        subject_report(user_input)
+        
     report_mode = False
